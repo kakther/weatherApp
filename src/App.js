83b186a045b3eff2a84e9a1d22782a936ff3b8a9
Kausar Akther <kausar.akther@gmail.com>
2020-05-27 01:17:38 -0400

    Add css

--- src/App.js
+++ src/App.js
@@ -20,12 +20,12 @@ class App extends React.Component{
     e.preventDefault();
     const city = e.target.elements.city.value;
     const country = e.target.elements.country.value;
-    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&APPID=${API_KEY}`); 
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&APPID=${API_KEY}&units=metric`); 
     const data = await api_call.json();
-    if (city && country){
-      // console.log(data);
+    if (city && country) {
+      
       this.setState({
-        temperature:data.main.temp,
+        temperature: data.main.temp,
         city: data.name,
         country: data.sys.country,
         humidity: data.main.humidity,
@@ -47,7 +47,6 @@ class App extends React.Component{
 render() {
     return (
       <div>
-      {/* <img src={Bacground}/> */}
         <div className="wrapper">
           <div className="main">
             <div className="container">
@@ -58,12 +57,12 @@ render() {
                 <div className="col-xs-7 form-container">
                 <Form getWeather={this.getWeather}/>
                 <Weather 
-          temperature={this.state.temperature}
-          city={this.state.city}
-          country={this.state.country}
-          humidity={this.state.humidity}
-          description={this.state.description}
-          error={this.state.error}
+                  temperature={this.state.temperature}
+                  city={this.state.city}
+                  country={this.state.country}
+                  humidity={this.state.humidity}
+                  description={this.state.description}
+                  error={this.state.error}
           />
               </div>   
               </div>
